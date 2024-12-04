fb03c24ef5e7bf450a4d79b40981348b9571d86e
Astroneat <datnvt2007@gmail.com>
2024-12-04 10:49:53 +0700

    Changed to CounterAPI

--- counter/script.js
+++ counter/script.js
@@ -1,23 +1,33 @@
-function update_count() {
+const api_address = 'https://api.counterapi.dev/v1';
+const namespace = 'astroneat-github.io';
+const key = 'counter-count';
+
+/*
+'req' can be:
+- "up"
+- "down"
+- "set"
+- "": means get
+- "list": idk what this does
+*/
+function fetch_counter(req) {
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://abacus.jasoncameron.dev/get/astroneat.github.io/counter-count");
+    xhr.open("GET", `${api_address}/${namespace}/${key}/${req}`);
+    console.log(`${api_address}/${namespace}/${key}/${req}`);
     xhr.responseType = "json";
     xhr.onload = function() {
-        document.getElementById('count').innerText = this.response.value;
-        console.log(this.response.value);
+        document.getElementById('count').textContent = this.response.count;
+        console.log(this.response.count);
     }
     xhr.send();
 }
 
+function update_count() {
+    fetch_counter("");
+}
+
 function countup() {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://abacus.jasoncameron.dev/hit/astroneat.github.io/counter-count");
-    xhr.responseType = "json";
-    xhr.onload = function() {
-        document.getElementById('count').innerText = this.response.value;
-        console.log(this.response.value);
-    }
-    xhr.send();
+    fetch_counter("up");
 
     // let current_count = parseInt(document.getElementById("count").innerHTML);
     // current_count += 1;

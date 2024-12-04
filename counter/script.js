e4043a3cf7a0aa28d1c0cc218de1fa3929f69a44
Astroneat <datnvt2007@gmail.com>
2024-12-04 10:11:26 +0700

    countapi babyy

--- counter/script.js
+++ counter/script.js
@@ -1,5 +1,25 @@
+function update_count() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://abacus.jasoncameron.dev/get/astroneat.github.io/counter-count");
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        document.getElementById('count').innerText = this.response.value;
+        console.log(this.response.value);
+    }
+    xhr.send();
+}
+
 function countup() {
-    let current_count = parseInt(document.getElementById("count").innerText);
-    current_count += 1;
-    document.getElementById("count").innerText = current_count;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://abacus.jasoncameron.dev/hit/astroneat.github.io/counter-count");
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        document.getElementById('count').innerText = this.response.value;
+        console.log(this.response.value);
+    }
+    xhr.send();
+
+    // let current_count = parseInt(document.getElementById("count").innerHTML);
+    // current_count += 1;
+    // document.getElementById("count").innerHTML = current_count;
 }
